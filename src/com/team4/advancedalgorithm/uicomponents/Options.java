603c793cbd8112cc34c379b661aa6817dac91a08
Nguyen Duc Khoi <ndkhoi168@gmail.com>
2012-01-06 15:51:17 +0700

    Add readme

--- src/com/team4/advancedalgorithm/uicomponents/Options.java
+++ src/com/team4/advancedalgorithm/uicomponents/Options.java
@@ -1,9 +1,12 @@
 package com.team4.advancedalgorithm.uicomponents;
 
-import java.awt.Button;
 import java.awt.Event;
 import java.awt.GridLayout;
-import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 import com.team4.advancedalgorithm.GraphAlgorithm;
 
@@ -11,20 +14,20 @@ import com.team4.advancedalgorithm.GraphAlgorithm;
  * @author <a href="mailto:ndkhoi168@gmail.com">Nguyen Duc Khoi</a>
  * @version $Revision$
  */
-public class Options extends Panel {
+public class Options extends JPanel implements ActionListener {
 	// set of options at the right of the screen
 
-	Button b1 = new Button("clear");
+	JButton b1 = new JButton("clear");
 
-	Button b2 = new Button("run");
+	JButton b2 = new JButton("run");
 
-	Button b3 = new Button("step");
+	JButton b3 = new JButton("step");
 
-	Button b4 = new Button("reset");
+	JButton b4 = new JButton("reset");
 
-	Button b5 = new Button("example");
+	JButton b5 = new JButton("example");
 
-	Button b6 = new Button("exit");
+	JButton b6 = new JButton("exit");
 
 	GraphAlgorithm parent;
 
@@ -36,23 +39,29 @@ public class Options extends Panel {
 
 		setLayout(new GridLayout(6, 1, 0, 10));
 
+		b1.addActionListener(this);
 		add(b1);
 
+		b2.addActionListener(this);
 		add(b2);
 
+		b3.addActionListener(this);
 		add(b3);
 
+		b4.addActionListener(this);
 		add(b4);
 
+		b5.addActionListener(this);
 		add(b5);
 
+		b6.addActionListener(this);
 		add(b6);
 
 	}
 
 	public boolean action(Event evt, Object arg) {
 
-		if (evt.target instanceof Button) {
+		if (evt.target instanceof JButton) {
 
 			if (((String) arg).equals("step")) {
 
@@ -139,5 +148,79 @@ public class Options extends Panel {
 
 		b3.setLabel("step");
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() instanceof JButton) {
+
+			if (e.getSource().equals(b3)) {
+
+				if (!Locked) {
+
+					b3.setLabel("next step");
+
+					parent.getGraphcanvas().stepalg();
+
+				}
+
+				else
+					parent.getDocumentation().doctext.showline("locked");
+
+			}
+
+			if (((JButton) e.getSource()).getText().equals("next step"))
+
+				parent.getGraphcanvas().nextstep();
+
+			if (((JButton) e.getSource()).getText().equals("reset")) {
+
+				parent.getGraphcanvas().reset();
+
+				b3.setLabel("step");
+
+				parent.getDocumentation().doctext.showline("all items");
+
+			}
+
+			if (((JButton) e.getSource()).getText().equals("clear")) {
+
+				parent.getGraphcanvas().clear();
+
+				b3.setLabel("step");
+
+				parent.getDocumentation().doctext.showline("all items");
+
+			}
+
+			if (((JButton) e.getSource()).getText().equals("run")) {
+
+				if (!Locked)
+
+					parent.getGraphcanvas().runalg();
+
+				else
+					parent.getDocumentation().doctext.showline("locked");
+
+			}
+
+			if (((JButton) e.getSource()).getText().equals("example")) {
+
+				if (!Locked)
+
+					parent.getGraphcanvas().showexample();
+
+				else
+					parent.getDocumentation().doctext.showline("locked");
+
+			}
+
+			if (((JButton) e.getSource()).getText().equals("exit")) {
+
+				System.exit(0);
+
+			}
+
+		}
 	}
 }
